a684024d71bf1e4f5f01e4ad24f4351e7bcf3b88
Nathan Ruffing <nate@rr34.us>
2024-10-28 04:44:08 +0000

    second commit. migrate to local machine

--- main1.py
+++ main1.py
@@ -1,5 +1,5 @@
 import sqlstatements
 
-sqlstatements.test()
+# sqlstatements.test()
 
 sqlstatements.hours_report(('2024-10-14', '2024-10-27'))
--- sqlstatements.py
+++ sqlstatements.py
@@ -22,30 +22,18 @@ def hours_report(date_range):
 
     date_range_strings = (datetime.datetime.strftime(date_range_dts[0], '%Y-%m-%d'), datetime.datetime.strftime(date_range_dts[1], '%Y-%m-%d'))
 
-    date_delta = (date_range_dts[1] - date_range_dts[0]).days
-    dates_list = [date_range_dts[0] + datetime.timedelta(days=x) for x in range(date_delta)]
+    # date_delta = (date_range_dts[1] - date_range_dts[0]).days
+    # dates_list = [date_range_dts[0] + datetime.timedelta(days=x) for x in range(date_delta)]
 
-    people_list = DBfunctions.sql_execute("""
-SELECT Person 
+    hours_report = DBfunctions.sql_execute("""
+SELECT *, TIME_TO_SEC(TIMEDIFF(log_clock.EndTime, log_clock.StartTime))/(60*60)-log_clock.BreakHours as hourscalc
 from log_clock
-WHERE WorkDay BETWEEN ? AND ?
-GROUP BY Person
-ORDER BY Person ;
+WHERE WorkDay BETWEEN ? AND ? ;
 """, date_range_strings)
+    
+    hours_report_df = pd.DataFrame(hours_report.fetchall())
+    hours_report_df.columns = [row[0] for row in hours_report.description]
 
-    table_lists = []
-    for person in people_list:
-        row = [person]
-        sql_tuple = date_range_strings + person
-        hours_row = DBfunctions.sql_execute("""
-SELECT TIME_TO_SEC(TIMEDIFF(log_clock.EndTime, log_clock.StartTime))/(60*60)-log_clock.BreakHours as hourscalc
-from log_clock
-WHERE WorkDay BETWEEN ? AND ?
-AND Person = ?;
-""", sql_tuple)
-        
-        hours_row = list(hours_row.fetchall())
-        row.append(hours_row)
-        table_lists += row
+    hours_report_pivoted = hours_report_df.pivot(columns='WorkDay', index='Person')
 
-    return True
+    return hours_report_pivoted
