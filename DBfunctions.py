d733dfc186677fbcd2a30c99cd178d97a73d72de
Nathan Ruffing <nate@rr34.us>
2024-10-28 02:05:54 -0500

    finally simply pivots

--- DBfunctions.py
+++ DBfunctions.py
@@ -1,16 +1,53 @@
 import sys
 import mariadb
+import mysql.connector
 
 # takes mysql text and a tuple of the user input values and returns the results in cursor format. 
 def sql_execute(text, user_input):
     try:
+        # conn = mariadb.connect(
+        #     user="carruffsite",
+        #     host="127.0.0.1",
+        #     port=3306,
+        #     database="carruffdb"
+        # )
         conn = mariadb.connect(
-            user="carruffsite",
-            host="127.0.0.1",
+            user="nate",
+            host="108.174.197.50",
             port=3306,
             database="carruffdb"
         )
     except mariadb.Error as e:
+        print(f"Error connecting to MariaDB platform: {e}")
+        sys.exit(1)
+
+    # Get cursor
+    cur = conn.cursor()
+
+    if user_input:
+        cur.execute(text, user_input)
+    else:
+        cur.execute(text)
+
+    # description = cur.description
+
+    result = cur.fetchall()
+    columns = [c[0] for c in cur.description]
+    cur.close()
+    conn.close()
+
+    return result, columns
+
+
+def sql_execute_pd(text, user_input):
+    try:
+        conn = mysql.connector.connect(
+            user="nate",
+            host="108.174.197.50",
+            port=3306,
+            database="carruffdb"
+        )
+    except mysql.connector.Error as e:
         print(f"Error connecting to MariaDB platform: {e}")
         sys.exit(1)
 
--- sqlstatements.py
+++ sqlstatements.py
@@ -1,3 +1,4 @@
+import os
 import DBfunctions
 import datetime
 import pandas as pd
@@ -22,18 +23,20 @@ def hours_report(date_range):
 
     date_range_strings = (datetime.datetime.strftime(date_range_dts[0], '%Y-%m-%d'), datetime.datetime.strftime(date_range_dts[1], '%Y-%m-%d'))
 
-    # date_delta = (date_range_dts[1] - date_range_dts[0]).days
-    # dates_list = [date_range_dts[0] + datetime.timedelta(days=x) for x in range(date_delta)]
+    date_delta = (date_range_dts[1] - date_range_dts[0]).days
+    dates_list = [date_range_dts[0] + datetime.timedelta(days=x) for x in range(date_delta)]
 
-    hours_report = DBfunctions.sql_execute("""
-SELECT *, TIME_TO_SEC(TIMEDIFF(log_clock.EndTime, log_clock.StartTime))/(60*60)-log_clock.BreakHours as hourscalc
+    hours, columns = DBfunctions.sql_execute("""
+SELECT Person , WorkDay , TIME_TO_SEC(TIMEDIFF(log_clock.EndTime, log_clock.StartTime))/(60*60)-log_clock.BreakHours as hourscalc
 from log_clock
-WHERE WorkDay BETWEEN ? AND ? ;
+WHERE WorkDay BETWEEN ? AND ?
+ORDER BY Person , WorkDay ;
 """, date_range_strings)
     
-    hours_report_df = pd.DataFrame(hours_report.fetchall())
-    hours_report_df.columns = [row[0] for row in hours_report.description]
+    hours_report = pd.DataFrame(hours, columns=columns)
 
-    hours_report_pivoted = hours_report_df.pivot(columns='WorkDay', index='Person')
+    hours_report_pivoted = hours_report.pivot(index='Person', columns='WorkDay')
 
-    return hours_report_pivoted
+    hours_report_pivoted.to_csv('hours_report.csv')
+    
+    return True
