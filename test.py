63e8f32f0e47a785e0541c010ca33f4bbb9d62bb
Nathan Ruffing <nate@rr34.us>
2024-11-11 04:33:31 -0500

    just changed test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import sqlstatements
 
 # sqlstatements.test()
 
-# sqlstatements.hours_report('2024-11-04')
+sqlstatements.hours_report('2024-11-11')
 
 # building = 'A'
 # rooms_list = () # if single room number, requires a comma after
@@ -15,5 +15,5 @@ import sqlstatements
 # note = 'Has new register on it. Looks new. NMR'
 # sqlstatements.change_status(building, rooms_list, item, status, note)
 
-print_this = sqlstatements.todo_list_room('A','428')
-print(print_this)
+# print_this = sqlstatements.todo_list_room('A','428')
+# print(print_this)
